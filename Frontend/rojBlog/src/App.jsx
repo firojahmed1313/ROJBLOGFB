49380079803173ebf6d5526d937a403f9c12c987
MD FIROJ AHMED <74416772+firojahmed1313@users.noreply.github.com>
2024-07-16 11:58:59 +0530

    Update App.jsx

--- Frontend/rojBlog/src/App.jsx
+++ Frontend/rojBlog/src/App.jsx
@@ -1,23 +1,23 @@
 import React, {lazy, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
-//import Home from "./pages/Home";
-//import Register from "./pages/Register";
-//import AddBlog from "./pages/AddBlog";
-//import Profile from "./pages/Profile";
-//import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import AddBlog from "./pages/AddBlog";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 import Navber from "./components/Navber";
-//import SaveBlog from "./pages/SaveBlog";
+import SaveBlog from "./pages/SaveBlog";
 import GetCookie from "./cookies/GetCookie";
 import context from "./context/Context";
-//import ForgotPassword from "./pages/ForgotPassword";
+import ForgotPassword from "./pages/ForgotPassword";
 
-const Home = lazy(() => import('./pages/Home'));
+/*const Home = lazy(() => import('./pages/Home'));
 const Register = lazy(() => import('./pages/Register'));
 const AddBlog = lazy(() => import('./pages/AddBlog'));
 const SaveBlog = lazy(() => import('./pages/SaveBlog'));
 const Profile = lazy(() => import('./pages/Profile'));
 const Login = lazy(() => import('./pages/Login'));
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));*/
 
 
 const App = () => {
